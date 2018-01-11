bc8a7c3eda466e74943089576424b2535e3af4d1
david <ariedov@gmail.com>
2018-01-11 23:53:51 +0200

    checks working flag for supervisor before changing

--- script/supervisor.js
+++ script/supervisor.js
@@ -2,11 +2,12 @@ const logger = require('./logger');
 
 module.exports = {
   supervise(creep, feed, work) {
-    if (creep.carry.energy === 0) {
+    const working = creep.memory.working;
+    if (working && creep.carry.energy === 0) {
       creep.memory.working = false;
       logger.logCreep(creep, 'now feeding');
     }
-    if (creep.carry.energy === creep.carryCapacity) {
+    if (!working && creep.carry.energy === creep.carryCapacity) {
       creep.memory.working = true;
       logger.logCreep(creep, 'now working');
     }
