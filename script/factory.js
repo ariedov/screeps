86ed5ab11da1cd2b8ef16202056d9d951b2831e8
david <ariedov@gmail.com>
2018-01-13 20:12:05 +0200

    build fix

--- script/factory.js
+++ script/factory.js
@@ -15,7 +15,7 @@ function getWorkerBodyparts(totalEnergy = 0) {
   const bodyparts = partsToAdd;
   let cost = BODYPART_COST[WORK] + BODYPART_COST[CARRY] + BODYPART_COST[MOVE];
   for (let i = 1, nextPart = partsToAdd[0]; cost + BODYPART_COST[nextPart] < totalEnergy; ++i) {
-    bodyparts.add(nextPart);
+    bodyparts.push(nextPart);
     cost += BODYPART_COST[nextPart];
     nextPart = partsToAdd[i % partsToAdd.length];
   }
