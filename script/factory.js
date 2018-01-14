425d760fbdfdf324189015a77a99fafaf6fa9410
david <ariedov@gmail.com>
2018-01-14 18:43:54 +0200

    adds maxPartsCount check to factory

--- script/factory.js
+++ script/factory.js
@@ -1,5 +1,7 @@
 const logger = require('./logger');
 
+const maxPartsCount = 50;
+
 module.exports = {
   createWorker(spawn, roomEnergy = 0, role = '') {
     const name = role.toUpperCase() + '_' + Game.time;
@@ -21,7 +23,7 @@ function getWorkerBodyparts(totalEnergy = 0) {
   const partsToAdd = [WORK, CARRY, MOVE];
   const bodyparts = partsToAdd;
   let cost = BODYPART_COST[WORK] + BODYPART_COST[CARRY] + BODYPART_COST[MOVE];
-  for (let i = 1, nextPart = partsToAdd[0]; cost + BODYPART_COST[nextPart] < totalEnergy; ++i) {
+  for (let i = 1, nextPart = partsToAdd[0]; cost + BODYPART_COST[nextPart] < totalEnergy && i < maxPartsCount; ++i) {
     bodyparts.push(nextPart);
     cost += BODYPART_COST[nextPart];
     nextPart = partsToAdd[i % partsToAdd.length];
