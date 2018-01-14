cdb843215cf6b8b38b0d15a5bee5ebec8484a6c9
david <ariedov@gmail.com>
2018-01-14 17:38:29 +0200

    logging creep creation

--- script/factory.js
+++ script/factory.js
@@ -1,7 +1,11 @@
+const logger = require('./logger');
+
 module.exports = {
   createWorker(spawn, roomEnergy = 0, role = '') {
     const name = role.toUpperCase() + '_' + Game.time;
-    return spawn.spawnCreep(getWorkerBodyparts(roomEnergy), name, {
+    const body = getWorkerBodyparts(roomEnergy);
+    logger.log('creep: ' + name + ' created with body ' + body + '. energy:' + roomEnergy);
+    return spawn.spawnCreep(body, name, {
       memory: {
         name,
         role
