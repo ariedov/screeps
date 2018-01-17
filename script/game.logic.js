f745e63d34e65c775a155796f8c0ff8e951fd5c9
David Leibovych <david.leibovych@x2sy.com>
2018-01-17 14:34:35 +0200

    handles creep empty source

--- script/game.logic.js
+++ script/game.logic.js
@@ -4,12 +4,14 @@ module.exports = {
 
   harvest(creep) {
     const source = balancer.getSourceForCreep(creep);
-    if (source.energy) {
-      if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+    if (source) {
+      if (source.energy) {
+        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+        }
+      } else if (creep.withdraw(source, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
         creep.moveTo(source);
       }
-    } else if (creep.withdraw(source, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-      creep.moveTo(source);
     }
   }
 };
